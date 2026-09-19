52fa47bd44beb4a0fb11ca07b9c03364fb112ef4
Daniel Wilds <247745881+w3bmstr@users.noreply.github.com>
2026-09-18 20:40:54 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'bodyos-v2.1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request)
+        .then((res) => {
+          if (res && res.ok && event.request.url.startsWith(self.location.origin)) {
+            const clone = res.clone();
+            caches.open(CACHE).then((c) => c.put(event.request, clone));
+          }
+          return res;
+        })
+        .catch(() => cached || Response.error());
+      return cached || fetched;
+    })
+  );
+});
